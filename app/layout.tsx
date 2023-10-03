d757280fee66e732197348ebb575e99670a08c67
idris chaalel <idrischaalel8@gmail.com>
2023-10-03 01:07:28 +0100

    feat: sidebar + initial modal + modal createserver

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -20,16 +23,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={cn(
-          font.className,
-          "bg-white dark:bg-[#313338]"
-          )}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             storageKey="team-chat-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
